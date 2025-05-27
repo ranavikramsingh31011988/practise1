8df07de33a0653689f8ba10ff1a35fdd0f0fbd3d
Aparna Ranjan <kumarir@sts.holdings>
2025-05-27 12:30:37 +0530

    nb

--- practise.py
+++ practise.py
@@ -1,3 +1,18 @@
-for i in range(5):
-    print(i)
-
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+class Mammal(Animal):
+    def breathe(self):
+        print("Mammal breathes")
+class Bird(Animal):
+    def fly(self):
+        print("Bird flies")
+class Bat(Mammal, Bird):  # Bat inherits from both Mammal and Bird
+    def hang(self):
+        print("Bat hangs upside down")
+# Creating an object of the Bat class
+bat = Bat()
+bat.speak()  # Inherited from Animal
+bat.breathe()  # Inherited from Mammal
+bat.fly()  # Inherited from Bird
+bat.hang()  # Defined in Bat
